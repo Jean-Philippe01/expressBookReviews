84575c0bdebd454ae1145d781483c58795d8f1b5
Jean-Philippe01 <jeanphilippe.neris@gmail.com>
2025-02-19 18:43:00 -0500

    code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,8 +47,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    book = books[req.params.isbn]
+    return res.send(JSON.stringify(book.reviews, null, 4));
 });
 
 module.exports.general = public_users;
